c62b3ba1b919ecf476a2df83ceedf3e09e37c521
nothern05 <gleb5shimko@gmail.com>
2019-12-27 20:48:43 +0200

    CustomerAccount project creating

---
+++ CustomerAccount/src/main/java/com/example/demo/CustomerAccountApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class CustomerAccountApplication {
+
+    public static void main(String[] args) {
+        run(CustomerAccountApplication.class, args);
+    }
+
+}
